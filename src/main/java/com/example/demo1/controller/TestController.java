f190be088c9775da5b406038ef56ded5396959df
klayQin <625222110@qq.com>
2019-11-01 11:29:48 +0800

    ceshi123

---
+++ src/main/java/com/example/demo1/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.example.demo1.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String test(){
+        //888888888888888888888888ok
+        return "123";
+    }
+}
